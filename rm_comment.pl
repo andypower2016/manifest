1b0dbb958974671099ba20e1d19509226ab46315
andypower2016 <73726447+andypower2016@users.noreply.github.com>
2022-07-22 20:20:08 +0800

    Create rm_comment.pl

---
+++ rm_comment.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+use Regexp::Common;
+#$string =~ s/$RE{comment}{C}//g;
+
+my $string = "/* Test program */ \n 
+// File \n
+// Loca \n
+ int main()  \n 
+ {           \n
+   // variable declaration \n
+   int a, b, c;  // 123  \n
+   /* This is a test  \n
+      multiline     \n
+      comment for   \n
+      testing */      \n
+  /* This is a test  \n
+   *   multiline     \n
+   *   comment for   \n
+   *   testing  \n
+   */ \n
+   int x = 123;    \n
+  /* *This is a test** */ \n
+   // *  multiline     \n
+   // *  comment for   \n
+   //  * testing       \n
+   a = b + c;       \n
+ } \n";
+
+my $rmComment = $string;
+$rmComment =~ s/((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))//g;
+
+printf("%s",$rmComment);
+
+exit 0;
